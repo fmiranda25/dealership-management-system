c32e2298a06da64915150722f7ea59095cc14409
Craig McKeachie <craigdmckeachie@gmail.com>
2026-05-08 16:11:44 -0400

    Add Program, Dealership, UserInterface and Vehicle

---
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -0,0 +1,12 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class UserInterface {
+    private Dealership dealership;
+    private Scanner scanner;
+
+    public void display(){
+        System.out.println("My dealership app started.");
+    }
+}
